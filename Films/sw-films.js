b99e347a0a33b85e66bcf2702f5b558c1571ef5c
DocSmudge <10971828@uvu.edu>
2021-10-19 09:16:47 -0600

    test

--- Films/sw-films.js
+++ Films/sw-films.js
@@ -2,5 +2,6 @@ import { films } from '../data/films.js'
 
 let filmList = document.querySelector('#filmList')
 
-filmList.textContent = 'This is my content'
+filmList.textContent = 'This is my content. here is more content'
+
 
