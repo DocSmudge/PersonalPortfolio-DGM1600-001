9f9d7b9fc46975984be728461d42e60a1ae30600
DocSmudge <10971828@uvu.edu>
2021-10-19 09:22:00 -0600

    img test

--- Films/sw-films.js
+++ Films/sw-films.js
@@ -4,4 +4,8 @@ let filmList = document.querySelector('#filmList')
 
 filmList.textContent = 'This is my content. here is more content'
 
+let myImg=document.createElement('img')
+myImg.src = 'https://starwars-visualguide.com/assests/img/films/1.jpg'
+filmList.appendChild('myImg')
+
 
