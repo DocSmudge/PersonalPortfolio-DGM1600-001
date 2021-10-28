ac15b11c3cc978ffc74b40cc21b3a2e96fb090c8
DocSmudge <10971828@uvu.edu>
2021-10-28 12:26:17 -0600

    edits from 10/26 class "Got buttons working"

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,9 +1,63 @@
-import { people } from'../data/people.js.js'
+import { people } from "../data/people.js";
 
-people.forEach(element => {
-    const charFigure = document.createElement('figure')
-    const charImg = document .createElement('img')
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
-    charFigure.appendChild(charImg)
-    mainContent.appendChild(charFigure)
+const mainContent = document.querySelector("#main");
+
+const maleCharacters = people.filter((person) => person.gender === "male");
+const femaleCharacters = people.filter((person) => person.gender === "female");
+const otherCharacters = people.filter((person) => {
+  if (
+    person.gender === "hermaphrodite" ||
+    person.gender === "n/a" ||
+    person.gender === "none"
+  ) {
+    return person;
+  }
+});
+
+const header = document.createElement("header");
+const maleButton = document.createElement("button");
+maleButton.textContent = "Male Characters";
+
+populateDOM(otherCharacters);
+
+maleButton.addEventListener("click", () => populateDOM(maleCharacters));
+
+const femaleButton = document.createElement("button");
+femaleButton.textContent = "Female Characters";
+
+femaleButton.addEventListener("click", () => populateDOM(femaleCharacters));
+
+header.appendChild(maleButton);
+header.appendChild(femaleButton);
+
+document.body.insertBefore(header, mainContent);
+
+function populateDOM(characters) {
+  while (mainContent.firstChild) {
+    mainContent.removeChild(mainContent.firstChild);
+  }
+
+  characters.forEach((element) => {
+    const charFigure = document.createElement("figure");
+    const charImg = document.createElement("img");
+    const charNum = getLastNumber(element.url);
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+
+    const charCaption = document.createElement("figcaption");
+    charCaption.textContent = element.name;
+
+    charFigure.appendChild(charImg);
+    charFigure.appendChild(charCaption);
+    mainContent.appendChild(charFigure);
+  });
+}
+
+function getLastNumber(url) {
+  let end = url.lastIndexOf("/");
+  let start = end - 2;
+
+  if (url.charAt(start) === "/") {
+    start++;
+  }
+  return url.slice(start, end);
 }
