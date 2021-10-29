fbb2de31d04db234273e2778ecf10c876ce92ad1
DocSmudge <10971828@uvu.edu>
2021-10-29 10:42:42 -0600

    .25 through 10/26

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+export function getLastNumber(url) {
+    let end = url.lastIndexOf("/");
+    let start = end - 2;
+  
+    if (url.charAt(start) === "/") {
+      start++;
+    }
+    return url.slice(start, end);
+  }
+  
