1dc79572dc8e930dcd071e6cc8845d59dd04703f
DocSmudge <10971828@uvu.edu>
2021-11-16 12:53:34 -0700

    Beautiful reduce function :)

--- congress/congress.js
+++ congress/congress.js
@@ -2,20 +2,20 @@ import { senators } from '../data/senators.js'
 
 const senatorDiv = document.querySelector('.senators')
 
-function SimplifiedSenators(senatorArray){
-    return senatorArray.map(senator => {
+function SimplifiedSenators(){
+    return senators.map(senator => {
         let middleName = senator.middle_name ? `${senator.middle_name}` : ` `
         return {
              id: senator.id,
              name: `${senator.first_name}${middleName}${senator.last_name}`,
              party: senator.party,
+             gender: senator.gender,
+             seniority: +senator.seniority,
              imgURL:`https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
+             
          }
     })
 }
-
-
-populateSenatorDiv(SimplifiedSenators(senators))
 
 function populateSenatorDiv(simpleSenators){
     simpleSenators.forEach(senator =>{
@@ -26,10 +26,18 @@ function populateSenatorDiv(simpleSenators){
         figImg.src = senator.imgURL
         figCaption.textContent = senator.name
         
-
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
         senatorDiv.appendChild(senFigure)
     })
 
 }
+const filterSenators = (prop, value) =>  SimplifiedSenators().filter(senator => senator[prop] === value)
+    
+    
+    console.log(filterSenators('gender', 'F'))
+
+     const mostSeniorSenator = SimplifiedSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+     console.log(mostSeniorSenator)
+    
+    populateSenatorDiv(SimplifiedSenators())
