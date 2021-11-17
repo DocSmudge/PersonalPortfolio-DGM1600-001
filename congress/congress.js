71d0fe65ba4d45d8bc3b18c505d35911dcba8a91
DocSmudge <10971828@uvu.edu>
2021-11-17 09:13:21 -0700

    First half of 11/16 lecture

--- congress/congress.js
+++ congress/congress.js
@@ -1,17 +1,27 @@
 import { senators } from '../data/senators.js'
+import { representatives } from '../data/representatives.js'
+
+
+const members = [...senators, ...representatives] //spread method - modern way to combine arrays like a genius! (not genus lol)
+
+console.log(members.length)
 
 const senatorDiv = document.querySelector('.senators')
 
-function SimplifiedSenators(){
+function SimplifiedMembers(chamberFilter){
+   const filter = members.filter(member => chamberFilter ? member.short_title === chamberFilter : members)
+    
     return senators.map(senator => {
         let middleName = senator.middle_name ? `${senator.middle_name}` : ` `
         return {
              id: senator.id,
-             name: `${senator.first_name}${middleName}${senator.last_name}`,
+             name: `${senator.first_name} ${middleName}${senator.last_name}`,
              party: senator.party,
              gender: senator.gender,
              seniority: +senator.seniority,
-             imgURL:`https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
+             imgURL:`https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+             missedVotesPct: senator.missed_votes_pct,
+             loyaltyPct: senator.votes_with_party_pct,
              
          }
     })
@@ -35,9 +45,21 @@ function populateSenatorDiv(simpleSenators){
 const filterSenators = (prop, value) =>  SimplifiedSenators().filter(senator => senator[prop] === value)
     
     
-    console.log(filterSenators('gender', 'F'))
+    //console.log(filterSenators('gender', 'F'))
 
      const mostSeniorSenator = SimplifiedSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
-     console.log(mostSeniorSenator)
+
+     //console.log(mostSeniorSenator)
+
+     const mostLoyal = SimplifiedSenators().reduce((acc, senator) => {
+         if (senator.loyaltyPct === 100) { 
+         acc.push(senator)
+     }
+     return acc
+    }, [])
     
+    //console.log(mostLoyal)
+
     populateSenatorDiv(SimplifiedSenators())
+
+    //pausing at 40:17 on 11/16 recording. He's right in the middle of solving the const filter issue. Double check to make sure line 12 matches his. Commit this first thing.
